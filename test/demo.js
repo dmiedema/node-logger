d31f37d7a286281b9d519101be3dcdcfe2a8f75d
Danny Siu <danny.siu@gmail.com>
2011-04-19 00:57:56 +0800

    removed redundant demo.js

--- test/demo.js
+++
@@ -1,6 +0,0 @@
-var logger = require('../lib/node-logger').logger(module, true);
-logger.info('Info message');
-logger.debug('Debug message');
-logger.warn('Warning message');
-logger.error('Error message');
-logger.trace('Trace message');
